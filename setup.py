1913e8c03bacd618e85b857509b8c27a476ea570
Benjmain Knapp <benjamin@knapp-programmieurng.de>
2024-10-08 15:42:39 +0200

    update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='number',
-    version='0.1',
+    version='0.2',
     scripts=['number.py'],
     author="otAAAh",
     author_email="benjamin@knapp-programmieurng.de",
